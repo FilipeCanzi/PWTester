a2b980b635c440acb7b3934f770d07feb406eb0c
Filipe Rogério Canzi da Silva <filipecanzi@gmail.com>
2024-01-01 20:53:49 -0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PWTester",
+    
+    platforms: [.iOS(.v17)],
+    
+    products: [
+        .library(
+            name: "PFoundation",
+            targets: ["PFoundation"]),
+        
+        .library(
+            name: "PInterface",
+            targets: ["PFoundation", "PInterface"]),
+        
+        .library(
+            name: "PMain",
+            targets: ["PMain", "PFoundation", "PInterface"])
+    ],
+    
+    targets: [
+        .binaryTarget(
+            name: "PFoundation",
+            path: "./Sources/PFoundation.xcframework"),
+        
+        .binaryTarget(
+            name: "PInterface",
+            path: "./Sources/PInterface.xcframework"),
+        
+        .binaryTarget(
+            name: "PMain",
+            path: "./Sources/PMain.xcframework"),
+    ]
+)
